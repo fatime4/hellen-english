a5116b46fa72e9fa193ebb3fd7c9111a18d72e9b
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-06-15 07:12:54 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -14,26 +14,54 @@ function myFunction() {
     moreText.style.display = 'inline';
   }
 }
-
+// slider
 $('.slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
+  arrows: true,
+  infinite: false,
   asNavFor: '.slider-nav',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+      },
+    },
+  ],
 });
 
 $('.slider-nav').slick({
   slidesToShow: 7,
-  slidesToScroll: 1,
+  slidesToScroll: 7,
   asNavFor: '.slider-for',
+  infinite: false,
   dots: false,
+  focusOnSelect: true,
   responsive: [
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 3,
+        slidesToScroll: 1,
         dots: false,
       },
     },
   ],
+});
+
+// button read more
+
+$(document).ready(function () {
+  $('.reviews__button').click(function () {
+    if ($('.reviews__button').val() == 1) {
+      $('.hide-text').show();
+      $('.reviews__button').html('Read less');
+      $('.reviews__button').val(2);
+    } else {
+      $('.hide-text').hide();
+      $('.reviews__button').html('Read more');
+      $('.reviews__button').val(1);
+    }
+  });
 });
