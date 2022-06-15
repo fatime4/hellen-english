1ae35522490c277afff0885f3a8f0b9252f68925
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-06-15 11:50:10 +0300

    fix slider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 //  button "read more"
-function myFunction() {
-  var dots = document.getElementById('dots');
-  var moreText = document.getElementById('more');
-  var btnText = document.getElementById('myBtn');
+// function myFunction() {
+//   var dots = document.getElementById('dots');
+//   var moreText = document.getElementById('more');
+//   var btnText = document.getElementById('myBtn');
 
-  if (dots.style.display === 'none') {
-    dots.style.display = 'inline';
-    btnText.innerHTML = 'Read more';
-    moreText.style.display = 'none';
-  } else {
-    dots.style.display = 'none';
-    btnText.innerHTML = 'Read less';
-    moreText.style.display = 'inline';
-  }
-}
+//   if (dots.style.display === 'none') {
+//     dots.style.display = 'inline';
+//     btnText.innerHTML = 'Read more';
+//     moreText.style.display = 'none';
+//   } else {
+//     dots.style.display = 'none';
+//     btnText.innerHTML = 'Read less';
+//     moreText.style.display = 'inline';
+//   }
+// }
 // slider
+
 $('.slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
